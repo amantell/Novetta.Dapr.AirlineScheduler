61b10221f3df5b75ac2952dd5da55f417d1d2605
amantell <adam_mantell@hotmail.com>
2021-10-04 16:41:39 -0400

    Added transaction functionality

--- AirlineScheduler/Program.cs
+++ AirlineScheduler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Dapr.Client;
 using Novetta.Dapr.AirlineScheduler.DataAccessLayer;
@@ -11,6 +12,46 @@ namespace Novetta.Dapr.AirlineScheduler
     {
         static async Task Main(string[] args)
         {
+//            await StateStoreTest();
+
+            await StateStoreTransactionTest();
+        }
+
+        private static async Task StateStoreTransactionTest() {
+            const string storeName = "statestore";
+
+            using var client = new DaprClientBuilder().Build();
+
+            // State transactions operate on raw bytes
+            var bytes = JsonSerializer.SerializeToUtf8Bytes(Metadata.Airlines);
+
+            var requests = new List<StateTransactionRequest>()
+            {
+                new StateTransactionRequest("airlines", bytes, StateOperationType.Upsert),
+                new StateTransactionRequest("oldairlines", null, StateOperationType.Delete)
+            };
+            
+            Console.WriteLine("Executing transaction - save state and delete state");
+
+            var cancellationToken = new CancellationToken();
+            await client.ExecuteStateTransactionAsync(storeName, requests, cancellationToken: cancellationToken);
+            Console.WriteLine("Executed State Transaction!");
+
+            var state = await client.GetStateAsync<Dictionary<string, string>>(storeName, "airlines", cancellationToken: cancellationToken);
+            if (state == null)
+            {
+                Console.WriteLine("State not found in store");
+            }
+            else
+            {
+                Console.WriteLine($"Transaction completed! Now iterate!");
+                foreach (KeyValuePair<string, string> keyValuePair in state) {
+                    Console.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value}");
+                }
+            }
+        }
+
+        private static async Task StateStoreTest() {
             const string storename = "statestore";
             string[] keys  = new string[4] { "airlines", "cities", "arrivals", "departures" };
             var daprClient = new DaprClientBuilder().Build();
